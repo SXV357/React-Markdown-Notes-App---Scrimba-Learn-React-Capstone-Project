9f4ec9cb71b341cbcf38a7f7c44d4eb88f1df6f6
Shreyas Viswanathan <shrevis2018@gmail.com>
2023-07-01 12:00:21 -0500

    cleanup + firebase config debugging

--- src/V2 - Firestore/App.js
+++ src/V2 - Firestore/App.js
@@ -8,27 +8,19 @@ import "../style.css"
 
 export default function App() {
   // addDoc and deleteDoc both return a promise so they need to be run asynchronously
-
-  // Attempt to get notes from local storage using lazy state initialization and if it doesn't exist, set it to an empty array
-
-  // we are no longer pulling notes from local storage and saving them in state
   const [notes, setNotes] = useState([]);
-    // () => {
-    // const storedNotes = JSON.parse(localStorage.getItem("notes"));
-    // return storedNotes !== null ? storedNotes : [];
-  // };
   const [tempNoteText, setTempNoteText] = useState("");
 
   // The note the user is currently editing
   // notes is starting off as an empty array so there is no need to do a conditional check to set the current note ID
   const [currentNoteId, setCurrentNoteId] = useState("");
 
+  const currentNote = notes.find((note) => note.id === currentNoteId) || notes[0];
+  const sortedNotesArray = notes.sort((a, b) => b.updatedAt - a.updatedAt);
+
   useEffect(() => {
     !currentNoteId && setCurrentNoteId(notes[0]?.id);
   }, [notes, currentNoteId])
-
-  const currentNote = notes.find((note) => note.id === currentNoteId) || notes[0];
-  const sortedNotesArray = notes.sort((a, b) => b.updatedAt - a.updatedAt);
 
   useEffect(() => {
     currentNote && setTempNoteText(currentNote.body);
@@ -53,7 +45,6 @@ export default function App() {
     // allow react to call the function when it needs to clean up any side effects as part of establishing the socket connection via the event listener
     return unsubscribe;
 
-    // localStorage.setItem("notes", JSON.stringify(notes));
   }, []); // we only want this listener to establish the connection when the component mounts
 
   // This is what the user is presented with whenever they create a new note
@@ -64,7 +55,6 @@ export default function App() {
       createdAt: Date.now(),
       updatedAt: Date.now()
     };
-    // setNotes((prevNotes) => [newNote, ...prevNotes]); // this is being handled by the snapshot already
     const newNoteRef = await addDoc(notesCollection, newNote);
     // specify which collection we want to add the note to and use that reference to set the id
     setCurrentNoteId(newNoteRef.id);
@@ -72,20 +62,9 @@ export default function App() {
 
   async function updateNote(text) {
     // this function pushes notes in which new changes have been made to the top of the notes list in the sidebae
-
     const docRef = doc(db, "notes", currentNoteId);
     await updateDoc(docRef, {body: text, updatedAt: Date.now()}, {merge: true});
     // every keystroke in the document is reflected in firebase
-
-    // setNotes((oldNotes) => {
-    //   const newArr = [];
-    //   oldNotes.forEach((note) => {
-    //     note.id === currentNoteId
-    //       ? newArr.unshift({ ...note, body: text }) // the one the user has made changes to(it gets pushed up)
-    //       : newArr.push(note);
-    //   });
-    //   return newArr;
-    // });
   }
 
   async function deleteNote(noteId) {
@@ -95,17 +74,6 @@ export default function App() {
       // the name of the notescollection and the id of the note to be deleted
     const currentDocRef = doc(db, "notes", noteId);
     await deleteDoc(currentDocRef);
-
-    // event.stopPropagation();
-    // setNotes((oldNotes) => {
-    //   const newArr = [];
-    //   oldNotes.forEach((note) => {
-    //     if (note.id !== noteId) {
-    //       newArr.push(note); // noteId represents the id of the note the user is trying to delete
-    //     }
-    //   });
-    //   return newArr;
-    // });
   }
 
   useEffect(() => {
