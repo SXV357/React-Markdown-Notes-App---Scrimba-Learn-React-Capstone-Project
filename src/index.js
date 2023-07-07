4cdcf20743336d56221d25136ac41d1c354abd27
Shreyas Viswanathan <shrevis2018@gmail.com>
2023-07-07 14:54:53 -0500

    maintain original version of project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './V2 - Firestore/App';
-import {BrowserRouter} from "react-router-dom"
-import AuthenticationProvider from './V2 - Firestore/AuthenticationProvider';
+import App from './App';
 
 ReactDOM.render(<React.StrictMode>
-  <BrowserRouter>
-    <AuthenticationProvider>
-      <App/>
-    </AuthenticationProvider>
-  </BrowserRouter>
+    <App/>
 </React.StrictMode>, document.getElementById('root'));
